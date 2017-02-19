3330b5be62bc0fedaf22a6423788ba3cc99fed7b
Tom Whitwell <tom.whitwell@gmail.com>
2017-02-19 21:47:01 +0000

    Update README.ml

--- Collateral/BOM/README.ml
+++ Collateral/BOM/README.ml
@@ -1,3 +1,1 @@
-Latest Turing 2 BOM is at:    
-
-[Turing 2 BOM in Google Docs](https://docs.google.com/spreadsheets/d/1FUUejrNd6qe-PUmlLi0BAgiGKWX9DxQH3Jt5qFOxPAQ/edit#gid=0)
+Latest Turing 2 BOM is in Google Docs: https://docs.google.com/spreadsheets/d/1FUUejrNd6qe-PUmlLi0BAgiGKWX9DxQH3Jt5qFOxPAQ/edit#gid=0 
