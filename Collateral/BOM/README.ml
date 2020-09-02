dbd3aeb7d4f6a36152bace6d84ad600c06c64436
Tom Whitwell <tom.whitwell@gmail.com>
2020-09-02 11:18:29 +0100

    Update README.ml

--- Collateral/BOM/README.ml
+++ Collateral/BOM/README.ml
@@ -1,1 +1,3 @@
-Latest Turing 2 BOM is in Google Docs: https://docs.google.com/spreadsheets/d/1FUUejrNd6qe-PUmlLi0BAgiGKWX9DxQH3Jt5qFOxPAQ/edit#gid=0 
+Latest Turing 2 BOM is in Google Docs: https://docs.google.com/spreadsheets/d/1FUUejrNd6qe-PUmlLi0BAgiGKWX9DxQH3Jt5qFOxPAQ/edit#gid=0   
+
+NB: Read note on not buying cheap semiconductors from eBay/Alibaba etc.  
